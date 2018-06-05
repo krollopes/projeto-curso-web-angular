d9ab470d23c07b009c377db9eacf1ad7aa47c238
Caroline <carolinelopes836@gmail.com>
2018-06-05 00:01:52 -0300

    intergração da página quem somos com firebase

--- src/app/services/information.service.ts
+++ src/app/services/information.service.ts
@@ -8,14 +8,30 @@ export class InformationService {
 
   info: any = {};
   carregado: boolean = false;
+  quem_somos:boolean = false;
+  equipe: any[] = [];
 
-  constructor( public http: Http ) { 
+  constructor( public http: Http ) {
 
+    this.carregar_info();
+    this.carregar_quem_somos();
+
+  }
+
+  public carregar_info() {
     this.http.get('assets/data/info.pagina.json')
               .subscribe( data => {
+              this.carregado = true;
+              this.info = data.json();
+    });
+  }
+
+  public carregar_quem_somos() {
+    this.http.get('https://pagina-web-angular.firebaseio.com/equipe.json')
+              .subscribe( data => {
                 console.log(data.json());
-                this.carregado = true;
-                this.info = data.json();
-              })
+              this.quem_somos = true;
+              this.equipe = data.json();
+    });
   }
 }
